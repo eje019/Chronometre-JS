841da66dc42d748d8bfc1c64c4576dc54f90c39e
eje019 <emricndayake@gmail.com>
2025-11-27 21:38:20 +0000

    initialiser les variables quqnd la page se charge

--- script.js
+++ script.js
@@ -3,5 +3,13 @@ var sp, btnStart, btnStop, btnReset, temps, ms, s, min, heure;
 
 //fonction pour initialiser les variables quqnd la page se charge
 window.onload = function() {
-    
+    sp=document.getElementsByTagName("span");
+    btnStart=document.getElementById("start");
+    btnStop=document.getElementById("stop");
+    btnReset=document.getElementById("reset");
+    temps = null;
+    ms = 0;
+    s = 0;
+    min = 0;
+    heure = 0;
 }
