df3d328b2d130f0ae66acc0eff6ae2a262c701c0
eje019 <emricndayake@gmail.com>
2025-11-27 21:34:20 +0000

    variables dont on a besoin

---
+++ script.js
@@ -0,0 +1,7 @@
+//les variables dont on a besoin 
+var sp, btnStart, btnStop, btnReset, temps, ms, s, min, heure;
+
+//fonction pour initialiser les variables quqnd la page se charge
+window.onload = function() {
+    
+}
